ced878d7996c85e0bb088f7d899ddda264c512b1
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-08-21 15:41:33 -0400

    Alterações para exibir o nome da categoria

--- Atividades/views.py
+++ Atividades/views.py
@@ -3,5 +3,6 @@ from django.http import JsonResponse
 from Atividades.models import Atividade
 
 def obter_todas_as_atividades(requisicao):
-	atividades = Atividade.objects.all().values('nome', 'descricao', 'id', 'url', 'endereco')
+	campos = ['nome', 'descricao', 'id', 'url', 'endereco', 'categoria__nome']
+	atividades = Atividade.objects.all().values(*campos)
 	return JsonResponse({'atividades': list(atividades)})
