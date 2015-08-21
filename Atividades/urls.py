dde72dd523c088edd95aa16747c1c46bb584d274
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-08-21 17:11:41 -0400

    Busca por categoria

--- Atividades/urls.py
+++ Atividades/urls.py
@@ -3,4 +3,5 @@ from Atividades import views
 
 urlpatterns = [
     url(r'^obter_todas_as_atividades/$', views.obter_todas_as_atividades, name="obter_todas_as_atividades"),
+    url(r'^atividades_de/(\w+)/$', views.atividades_por_categoria),
 ]
--- Atividades/views.py
+++ Atividades/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from Atividades.models import Atividade
+from Atividades.models import Categoria
 import random
 
 def obter_todas_as_atividades(requisicao):
+	return obter_atividades(Atividade.objects.all())
+
+def atividades_por_categoria(requisicao, categoria):
+	categoria = Categoria.objects.get(nome__iexact=categoria)
+	atividades = Atividade.objects.filter(categoria=categoria)
+	return obter_atividades(atividades)
+
+def obter_atividades(dados):
 	campos = ['nome', 'descricao', 'id', 'url', 'endereco', 'categoria__nome']
-	atividades = list(Atividade.objects.all().values(*campos))
+	atividades = list(dados.values(*campos))
 	random.shuffle(atividades)
 	return JsonResponse({'atividades': atividades})
