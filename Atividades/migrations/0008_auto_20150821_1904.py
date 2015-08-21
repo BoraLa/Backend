806998e156ebc4026626bf57bb234f58ca81f6ae
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-08-21 15:21:13 -0400

    Endereco na atividade

--- Atividades/migrations/0008_auto_20150821_1904.py
+++ Atividades/migrations/0008_auto_20150821_1904.py
@@ -34,5 +34,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-    	migrations.RunPython(lancar_atividades)    
+    	#migrations.RunPython(lancar_atividades)    
     ]
--- Atividades/views.py
+++ Atividades/views.py
@@ -3,5 +3,5 @@ from django.http import JsonResponse
 from Atividades.models import Atividade
 
 def obter_todas_as_atividades(requisicao):
-	atividades = Atividade.objects.all().values('nome', 'descricao', 'id', 'url')
+	atividades = Atividade.objects.all().values('nome', 'descricao', 'id', 'url', 'endereco')
 	return JsonResponse({'atividades': list(atividades)})
